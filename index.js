ca1911c98a8430d1cda2cc0f18656a2b3abdd9d4
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2022-07-06 14:18:19 -0400

    player class is done

--- index.js
+++ index.js
@@ -45,6 +45,18 @@ let p2HealthDiv = document.getElementById("p2Health");
 const updateGame = (p1, p2, gameState) => {
   // Update the DOM with the names and the latest health of players
   // Condition IF either player health is <= 0 then set isOver to true and declareWinner
+
+  p1NameDiv.innerText = p1.name;
+  p2NameDiv.innerText = p2.name;
+  p1HealthDiv.innerText = p1.health;
+  p2HealthDiv.innerText = p2.health;
+
+  if (p1.health <= 0 || p2.health <= 0) {
+    game.isOver = true;
+    gameState = game.isOver;
+    resultDiv.innerText = game.declareWinner(game.isOver, p1, p2);
+    return gameState;
+  }
 };
 
 // ** Create the Player class which can create a player with all it's attributes and methods **
@@ -61,16 +73,24 @@ class Player {
   // ** Attack an enemy with a random number from 0 to YOUR attackDmg bonus **
   strike(player, enemy, attackDmg) {
     // Get random number between 1 - 10 and that is damageAmount
+    let damageAmount = Math.floor(Math.random() * attackDmg);
     // Subtract the enemy health with the damageAmount
+    enemy.health -= damageAmount;
     //  Update the game and DOM with updateGame()
+    updateGame(player, enemy, game.isOver);
     //  Return a message of 'player name attacks enemy name for damageAmount'
+    return `${player.name} attacks ${enemy.name}`;
   }
   // ** Heal the player for random number from  1 to 5 **
   heal(player) {
     // Get random number between 1 - 5 and store that in hpAmount
+    let hpAmount = Math.floor(Math.random() * 5);
     // Add hpAmount to players health
+    player.health += hpAmount;
     //  Update the game and DOM with updateGame()
+    updateGame(p1, p2, game.isOver);
     //  Return a message of 'player name heals for hpAmount HP'
+    return `${player.name} heals for ${hpAmount} HP`;
   }
 }
 
@@ -109,17 +129,21 @@ class Game {
 }
 
 // ** Create 2 players using the player class **
-
+let player1 = new Player("yub", 100, 10);
+let player2 = new Player("ghi", 95, 10);
 // ** Save original Player Data into a variable in order to reset **
-let p1;
-let p2;
+let p1 = player1;
+let p2 = player2;
 
 // ** Create the game object from the Game class **
+let game = new Game();
 
 // ** Intialize the game by calling updateGame() **
 
+updateGame(p1, p2, game.isOver);
+
 // ** Save intial isOver from the game object inside this variable **
-let gameState;
+let gameState = game;
 
 // ** Add a click listener to the simulate button that runs the play() method on click and pass in the players **
 
